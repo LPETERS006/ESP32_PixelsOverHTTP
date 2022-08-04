2801698456664b22819c2abdf0ec92694919828d
Lasse <petersen@aht-group.com>
2022-08-04 15:53:46 +0200

    Update ESP32_PixelsOverHTTP.ino
    revert to single LED array (Matrix things now handled on server site) / add LetsEncrypt RootX1

--- ESP32_PixelsOverHTTP.ino
+++ ESP32_PixelsOverHTTP.ino
@@ -11,27 +11,60 @@
 #include <FastLED.h>
 #include <LinkedList.h>
 
-const uint8_t kMatrixWidth  = 30;
-const uint8_t kMatrixHeight = 20;
-bool kMatrixSerpentineLayout = true;
-#define NUM_LEDS (kMatrixWidth * kMatrixHeight)
-#define MAX_DIMENSION ((kMatrixWidth>kMatrixHeight) ? kMatrixWidth : kMatrixHeight)
+#define NUM_LEDS 192
 #define DATA_PIN 23
 CRGB leds[NUM_LEDS];
 
-const char* kSsid                  = "xxx";
-const char* kPassword              = "xxx";
-const char* kHostname              = "www.xxx.com";
+const char* kSsid                  = "WIFI-SSID";				/*Enter your SSID*/
+const char* kPassword              = "WIFI-PASS";				/*Enter your PASS*/
+const char* kHostname              = "www.example.com";			/*Enter your HOST*/
 const int kPort                    = 443;
-const char* kPath                  = "/xxx/xxx/xxx/";
+const char* kPath                  = "/main/path/rest/iot/matrix/";	/*Enter your path to restservice (we automatic the DEVICE MAC as URL like /main/path/rest/iot/matrix/FF00...)*/
 const int kNetworkTimeout          = 30 * 1000;
 const int kNetworkDelay            = 1000;
 unsigned long kPreviousTime        = 0;
 unsigned long kReconnectDelay      = 20000;
-const int kBlinkDelayMultiplicator = 5;
+const int kBlinkDelayMultiplicator = 3;
 const int kBlinkRepeats            = 5;
 int kLoopCount                     = 0;
 String kDeviceMac;
+
+
+/*LetsEncrypt Root X1 Cert for SSL*/
+
+const char cert_XSRG_ROOT_X1[] PROGMEM = R"CERT(
+-----BEGIN CERTIFICATE-----
+MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw
+TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh
+cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4
+WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu
+ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY
+MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc
+h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+
+0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U
+A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW
+T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH
+B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC
+B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv
+KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn
+OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn
+jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw
+qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI
+rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV
+HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq
+hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL
+ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ
+3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK
+NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5
+ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur
+TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC
+jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc
+oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq
+4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA
+mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d
+emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=
+-----END CERTIFICATE-----
+)CERT";
 
 WiFiClientSecure kClient;
 HttpClient http(kClient, kHostname, kPort);
@@ -45,7 +78,8 @@ LinkedList<int> kLinkedListIndex = LinkedList<int>();
   ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
 void initWiFi() 
 {
-  kClient.setInsecure();
+  /*kClient.setInsecure();*/
+  kClient.setCACert(cert_XSRG_ROOT_X1);
   WiFi.mode(WIFI_STA); WiFi.begin(kSsid, kPassword);
   while (WiFi.status() != WL_CONNECTED) { delay(500); Serial.print('.'); }
   Serial.println(WiFi.localIP());
@@ -100,13 +134,13 @@ void response2LED(String rData)
   {
     String rLED = getValues(rData, ',' , i);
   Serial.println(rLED);
-    if (leds[convert(i)] == CRGB( rLED.substring(1, 4).toInt(), rLED.substring(4, 7).toInt(), rLED.substring(7, 10).toInt())) {}
+    if (leds[i] == CRGB( rLED.substring(1, 4).toInt(), rLED.substring(4, 7).toInt(), rLED.substring(7, 10).toInt())) {}
     else 
   {
       Serial.println('.');
       kLinkedListIndex.add(i); kLinkedListOld.add(leds[i]);
       kLinkedListNew.add(CRGB( rLED.substring(1, 4).toInt(), rLED.substring(4, 7).toInt(), rLED.substring(7, 10).toInt()));
-      leds[convert(i)] = CRGB( rLED.substring(1, 4).toInt(), rLED.substring(4, 7).toInt(), rLED.substring(7, 10).toInt());
+      leds[i] = CRGB( rLED.substring(1, 4).toInt(), rLED.substring(4, 7).toInt(), rLED.substring(7, 10).toInt());
     }
   };
   FastLED.show();
@@ -183,44 +217,14 @@ void blink()
   Serial.println('blink'); Serial.println( 'index size' + String(kLinkedListIndex.size()));
   for (int i = 0; i < kLinkedListIndex.size(); i++)
   {
-    int l = kLinkedListIndex.get(i); Serial.println(String(i)); leds[convert(l)] = kLinkedListOld.get(i);
+    int l = kLinkedListIndex.get(i); Serial.println(String(i)); leds[l] = kLinkedListOld.get(i);
   };
   LEDS.show(); delay(100*kBlinkDelayMultiplicator);
   for (int i = 0; i < kLinkedListIndex.size(); i++) 
   {
-    int l = kLinkedListIndex.get(i); Serial.println(String(i)); leds[convert(l)] = kLinkedListNew.get(i);
+    int l = kLinkedListIndex.get(i); Serial.println(String(i)); leds[l] = kLinkedListNew.get(i);
   };
   LEDS.show(); delay(500*kBlinkDelayMultiplicator);
-}
-
-/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  ++ Convert 2 Matrix XY.....
-  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
-uint16_t convert(int i)
-{
-    uint8_t nr = i + 1;
-    uint8_t x = floor(nr/kMatrixWidth);
-    uint8_t y = nr % kMatrixWidth;
-    return (XY(x,y));
-}
-
-/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  ++ Matrix LEDs.....
-  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
-uint16_t XY( uint8_t x, uint8_t y)
-{
-  if (kMatrixSerpentineLayout == true) {
-    if ( y & 0x01) {
-      uint8_t reverseX = (kMatrixWidth - 1) - x;
-      return (y * kMatrixWidth) + reverseX;
-    }
-    else {
-      return (y * kMatrixWidth) + x;
-    }
-  }
-  if (kMatrixSerpentineLayout == false) {
-    return (y * kMatrixWidth) + x;
-  }
 }
 
 /*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
